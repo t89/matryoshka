dfe192c54cfef26fa7caeea0f0e152629ebb430b
Thomas Johannesmeyer <thomas@geeky.gent>
2019-05-08 11:44:56 +0200

    Add submodule managing script

---
+++ handle_sub.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+##
+#  handle_sub.sh
+#  Matryoshka
+#
+#  Created by Thomas Johannesmeyer (thomas@geeky.gent) on 08.04.2019.
+#  Copyright (c) 2019 www.geeky.gent. All rights reserved.
+#
+# This script is executed for each submodule. Git and terminal commands
+# behave relative to the _submodule's root_.
+# It takes one parameter (bool) which toggles auto-committing.
+
+
+# assign parameters
+should_autocommit="$1"
+
+# Let's see if an update is necessary
+git fetch
+
+# Kept for debugging purposes
+# head_sha1="$(git rev-parse HEAD)"
+head_commit_msg="$(git rev-list --format=%B --max-count=1 $sha1)"
+active_branch="$(git rev-parse --abbrev-ref HEAD)"
+upstream_status="$(git log HEAD..origin/$active_branch --oneline)"
+
+echo "\n  > Active branch: $active_branch\n  > HEAD: $head_commit_msg\n\n"
+
+# $upstream_status is empty unless changes on origin are available
+if [ ! "$upstream_status" = "" ]; then
+
+  git pull origin "$active_branch"
+
+  updated_hash="$(git rev-parse HEAD)"
+  shortened_hash="$(git rev-parse --short "$updated_hash")"
+
+  if [ $should_autocommit = "1" ]; then
+
+    # Reference submodule new head-sha1 in commit msg
+    commit_msg="Update $name to $shortened_hash"
+    echo "\n  > Committing: $commit_msg"
+
+    # Move cwd out of submodule into containing repo
+    cd ..
+
+    git add .
+    git commit -m "$commit_msg"
+
+  fi
+
+else
+  echo "  > Already up to date."
+fi
+
+exit 0
---
+++ matryoshka.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+##
+#  matryoshka.sh
+#  Matryoshka
+#
+#  Created by Thomas Johannesmeyer (thomas@geeky.gent) on 08.04.2019.
+#  Copyright (c) 2019 www.geeky.gent. All rights reserved.
+#
+# Update all submodules iteratively, and create concise referencing commits
+# for each update. Uses a helper script which is iterated within each submodule
+
+
+# Number of files added to the index (but uncommitted)
+staged_count="$(git status --porcelain 2>/dev/null| grep "^M" | wc -l)"
+
+# Number of files that are uncommitted and not added
+untracked_count="$(git status --porcelain 2>/dev/null| grep "^ M" | wc -l)"
+
+# Number of total uncommited files
+total_count="$(git status --porcelain 2>/dev/null| egrep "^(M| M)" | wc -l)"
+
+auto_commit=0
+did_stash=0
+
+# Ask for confirmation before auto-committing
+echo "You are attempting a submodule auto-update. You can cancel by pressing ctrl-c.\n\n"
+echo "Would you like to generate an auto-commit?\n"
+select yn in "Yes" "No"; do
+  case $yn in
+    Yes ) auto_commit=1; break;;
+    No ) auto_commit=0; break;;
+  esac
+done
+
+# Debug-Log kept for future reference
+# echo $staged_count
+# echo $untracked_count
+# echo $total_count
+
+if ! [ $staged_count -eq 0 -a $untracked_count -eq 0 -a $total_count -eq 0 ]; then
+  # Dirty Working Area
+
+  if [[ $auto_commit = 1 ]]; then
+
+    # We want to auto-commit cleanly. Stashing user changes...
+    echo "\nDirty working dir. Autostashing.\n"
+
+    # Stash save (q)uietly, including (u)ntracked files, also adding a description
+    git stash save --quiet --include-untracked "Submodule update `date`"
+
+    stash_sha1="$(git rev-parse stash@{0})"
+    echo "\nAutostashed working directory sha1: $stash_sha1\n"
+    did_stash=1
+  fi
+
+fi
+
+##
+# Getting absolute script path — this is not as trivial as you may think
+# REFERENCE: https://stackoverflow.com/q/4774054
+containing_dir_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Calling the update script for each sub. Passing the auto_commit flag
+git submodule foreach "sh $containing_dir_path/handle_sub.sh $auto_commit || :"
+
+# Kept for debugging purposes
+# git submodule foreach 'echo $path `git rev-parse HEAD` || :'
+
+# Reapply stashed changes
+if [[ $did_stash = 1 ]]; then
+  echo "\nReapplying stash\n"
+  git stash pop
+fi
+
+exit 0
+
